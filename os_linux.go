1c37ab6c3e558bf5ffff6f1393cfb89f12349149
shanebarnes <shanebarnes@users.noreply.github.com>
2021-02-10 15:04:26 -0500

    Debug Linux use of O_DIRECT

--- os_linux.go
+++ os_linux.go
@@ -10,17 +10,23 @@ import (
 
 func OpenFileRd(name string, perm os.FileMode) (*os.File, error) {
 	flag := os.O_RDONLY | os.O_SYNC
-	if !IsSpecialFile(name) {
-		flag |= syscall.O_DIRECT
+
+	// O_DIRECT may fail on some kernels and filesystems
+	f, err := os.OpenFile(name, flag | syscall.O_DIRECT, perm)
+	if err == syscall.EINVAL {
+		f, err = os.OpenFile(name, flag, perm)
 	}
-	return os.OpenFile(name, flag, perm)
+	return f, err
 }
 
 func OpenFileWr(name string, perm os.FileMode) (*os.File, error) {
 	// syscall.O_SYNC - wait for file data and meta data to be written to disk
 	flag := os.O_WRONLY | os.O_CREATE | os.O_TRUNC | syscall.O_DSYNC
-	if !IsSpecialFile(name) {
-		flag |= syscall.O_DIRECT
+
+	// O_DIRECT may fail on some kernels and filesystems
+	f, err := os.OpenFile(name,  flag | syscall.O_DIRECT, perm)
+	if err == syscall.EINVAL {
+		f, err = os.OpenFile(name, flag, perm)
 	}
-	return os.OpenFile(name,  flag, perm)
+	return f, err
 }
